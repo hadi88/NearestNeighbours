6038081995b06b0ff4d97b9fe83944678b8fc614
Hadi <hadi@Hadis-MacBook-Pro.local>
2013-04-01 10:03:42 -0700

    Initial Commit

---
+++ Near Neighbours/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Near Neighbours
+//
+//  Created by Hadi on 4/1/13.
+//  Copyright (c) 2013 Hadi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
